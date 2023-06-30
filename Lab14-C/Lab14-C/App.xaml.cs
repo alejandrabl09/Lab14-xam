69e13a085ab615bf79aafa19031dab7fd6f7effb
alejandrabl09 <aleblasdc09@gmail.com>
2023-06-30 17:14:54 -0500

    first commit

---
+++ Lab14-C/Lab14-C/App.xaml.cs
@@ -0,0 +1,32 @@
+﻿using Lab14_C.Services;
+using Lab14_C.Views;
+using System;
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace Lab14_C
+{
+    public partial class App : Application
+    {
+
+        public App()
+        {
+            InitializeComponent();
+
+            DependencyService.Register<MockDataStore>();
+            MainPage = new AppShell();
+        }
+
+        protected override void OnStart()
+        {
+        }
+
+        protected override void OnSleep()
+        {
+        }
+
+        protected override void OnResume()
+        {
+        }
+    }
+}
---
+++ Lab14-C/Lab14-C/AppShell.xaml.cs
@@ -0,0 +1,23 @@
+﻿using Lab14_C.ViewModels;
+using Lab14_C.Views;
+using System;
+using System.Collections.Generic;
+using Xamarin.Forms;
+
+namespace Lab14_C
+{
+    public partial class AppShell : Xamarin.Forms.Shell
+    {
+        public AppShell()
+        {
+            InitializeComponent();
+            Routing.RegisterRoute(nameof(ItemDetailPage), typeof(ItemDetailPage));
+            Routing.RegisterRoute(nameof(NewItemPage), typeof(NewItemPage));
+        }
+
+        private async void OnMenuItemClicked(object sender, EventArgs e)
+        {
+            await Shell.Current.GoToAsync("//LoginPage");
+        }
+    }
+}
